fa08d0abf087a8312980703deff4623b4c98bcc4
Ricardo Rivas <richistron@gmail.com>
2018-10-29 21:20:52 -0600

    this is not fast

--- lib/repeated_string.rb
+++ lib/repeated_string.rb
@@ -1,70 +1,21 @@
-
-class ListNode
-  def initialize(data)
-    @data = data
-    @next = nil
-  end
-
-  def set_next(node)
-    @next = node
-  end
-
-  def next
-    @next
-  end
-
-  def data
-    @data
-  end
-end
-
-class LinkedList
-  def initialize(items = [], listNode = ListNode)
-    @listNode = listNode
-    @head = nil
-    items.each do |item|
-      add_node item
-    end
-  end
-
-  def add_node(value)
-    if @head.nil?
-      @head = @listNode.new value
-    else
-      get_tail.set_next @listNode.new value
-    end
-  end
-
-  def head
-    @head
-  end
-
-  def size
-    count = 0
-    get_tail { count += 1}
-    count
-  end
-
-  private
-    def get_tail
-      current_node = @head
-      while current_node.next
-        current_node = current_node.next
-        yield current_node if block_given?
-      end
-      yield current_node if block_given?
-      current_node
-    end
-end
-
 class RepeatedString
   def initialize(str = '', n = 0)
     @size = n
-    @str = str
+    @arr = str.split ''
   end
 
   def count
-    res = @str.scan('a').size.to_f * @size.to_f / @str.size.to_f
-    res.round
+    @res = ''
+    @key = 0
+    @size.times do
+      if @arr[@key].nil?
+        @key = 0
+        @res += @arr[@key]
+      else
+        @res += @arr[@key]
+        @key += 1
+      end
+    end
+    @res.scan('a').size
   end
 end
--- spec/repeated_string_spec.rb
+++ spec/repeated_string_spec.rb
@@ -1,22 +1,5 @@
 require 'repeated_string'
 
-describe 'LinkedList' do
-  it 'a and b' do
-    list = LinkedList.new ['a', 'b']
-    expect(list.head.data).to eq('a')
-    expect(list.head.next.data).to eq('b')
-    expect(list.size).to eq(2)
-  end
-
-  it 'a, b and c' do
-    list = LinkedList.new ['a', 'b', 'c']
-    expect(list.head.data).to eq('a')
-    expect(list.head.next.data).to eq('b')
-    expect(list.head.next.next.data).to eq('c')
-    expect(list.size).to eq(3)
-  end
-end
-
 describe 'RepeatedString' do
   it 'case 1' do
     repeatedList = RepeatedString.new'aba', 10
@@ -28,8 +11,8 @@ describe 'RepeatedString' do
     expect(repeatedList.count).to be(1000000000000)
   end
 
-  # gfcaaaecbg
-  # 547602
-  #
-  # res 164280
+  it 'case 3' do
+    repeatedList = RepeatedString.new'gfcaaaecbg', 547602
+    expect(repeatedList.count).to be(164280)
+  end
 end
